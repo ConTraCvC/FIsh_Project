efabb970b17cb74cc970332401875482e5783021
cdnph <cdn11phuoc@gmail.com>
2023-03-20 23:56:21 +0700

    oke

--- src/pages/admin-page.jsx
+++ src/pages/admin-page.jsx
@@ -8,7 +8,7 @@ import { ref, uploadBytes} from "firebase/storage";
 import { store } from '@component/firebase/firebase'
 import { Modal, Box } from '@mui/material'
 import { db } from "@component/firebase/firebase"
-import { addDoc, collection } from "firebase/firestore/lite"
+import { addDoc, collection, doc, setDoc } from "firebase/firestore/lite"
 
 const style = {
   position: 'absolute',
@@ -47,8 +47,32 @@ export default function Admin() {
   const [title, setTitle] = useState("");
   const [title2, setTitle2] = useState("");
   const [body, setBody] = useState("");
+  const [body1, setBody1] = useState("");
   const [body2, setBody2] = useState("");
   const [body3, setBody3] = useState("");
+  const [don_gia, setDon_gia] = useState(0);
+  const [youtube, setYoutube] = useState("");
+  const [youtube1, setYoutube1] = useState("");
+  const [youtube2, setYoutube2] = useState("");
+  const [youtube3, setYoutube3] = useState("");
+  const [youtube4, setYoutube4] = useState("");
+  const [youtube5, setYoutube5] = useState("");
+  const [gia_thuong_pham, setGia_thuong_pham] = useState("");
+  const [gia_tri_dd, setGia_tri_dd] = useState("");
+  const [hinh_thuc_nuoi, setHinh_thuc_nuoi] = useState("");
+  const [hs_loi_nhuan, setHs_loi_nhuan] = useState("");
+  const [hscd_thuc_an, setHscd_thuc_an] = useState("");
+  const [kich_co, setKich_co] = useState("");
+  const [kich_thuoc, setKich_thuoc] = useState("");
+  const [loai_thuc_an, setLoai_thuc_an] = useState("");
+  const [mat_do_tha, setMat_do_tha] = useState("");
+  const [ten_goi, setTen_goi] = useState("");
+  const [thoi_gian_nuoi, setThoi_gian_nuoi] = useState("")
+  const [thuc_tai_sx, setThuc_tai_sx] = useState("")
+  const [ti_le_song, setTi_le_song] = useState("")
+  const [tieu_chuan, setTieu_chuan] = useState("")
+  const [vung_nuoi, setVung_nuoi] = useState("")
+  const [xu_huong_pt, setXu_huong_pt] = useState("")
 
   const handleClose = () => setIsOpen(false)
   const handleClose3 = () => setIsOpen3(false)
@@ -104,12 +128,17 @@ export default function Admin() {
   const imageRef = collection(db, 'news');
   const techRef = collection(db, 'techniques');
   const foodRef = collection(db, 'foods');
-  const fish_SX = collection(db, 'fish_types');
+  const fish_SX = collection(db, 'fish_SX');
   const fish_TN = collection(db, 'fish_TN');
+  // const fish_TN_details = collection(db, 'fish_TN', 'details');
   const fish_MU = collection(db, 'fish_MU');
+  // const fish_MU_details = collection(db, 'fish_MU', 'details');
   const fish_TC = collection(db, 'fish_TC');
+  // const fish_TC_details = collection(db, 'fish_TC', 'details');
   const fish_NT = collection(db, 'fish_NT');
+  // const fish_NT_details = collection(db, 'fish_NT', 'details');
   const fish_NN = collection(db, 'fish_NN');
+  // const fish_NN_details = collection(db, 'fish_NN', 'details');
 
   const uploadImages = async() => {
     if (imageUpload == null) {
@@ -191,11 +220,39 @@ export default function Admin() {
         alert("Image Uploaded")
       })
       await addDoc(fish_SX, {
-          title: {title},
-          body: {body},
-          image_tp: `https://firebasestorage.googleapis.com/v0/b/fishshop-80d05.appspot.com/o/fishs%2F${image_tp.name}?alt=media`
+        title: {title},
+        body: {body},
+        image_tp: `https://firebasestorage.googleapis.com/v0/b/fishshop-80d05.appspot.com/o/fishs%2F${image_tp.name}?alt=media`,
+        don_gia: {don_gia},
+        body1: {body1},
+        image: `https://firebasestorage.googleapis.com/v0/b/fishshop-80d05.appspot.com/o/fishs%2F${image.name}?alt=media`,
+        image1: `https://firebasestorage.googleapis.com/v0/b/fishshop-80d05.appspot.com/o/fishs%2F${image1.name}?alt=media`,
+        image2: `https://firebasestorage.googleapis.com/v0/b/fishshop-80d05.appspot.com/o/fishs%2F${image2.name}?alt=media`,
+        image3: `https://firebasestorage.googleapis.com/v0/b/fishshop-80d05.appspot.com/o/fishs%2F${image3.name}?alt=media`,
+        image4: `https://firebasestorage.googleapis.com/v0/b/fishshop-80d05.appspot.com/o/fishs%2F${image4.name}?alt=media`,
+        title2: {title2},
+        body2: {body2},
+        youtube: {youtube},
+        youtube1: {youtube1},
+        youtube2: {youtube2},
+        youtube3: {youtube3},
+        youtube4: {youtube4},
+        youtube5: {youtube5},
+        body3: {body3}
       }).then(() => {
         alert("Data Pushed")
+      })
+      await setDoc(doc(db, 'fish_SX', "details"), {
+        gia_thuong_pham: {gia_thuong_pham},
+        gia_tri_dd: {gia_tri_dd},
+        hinh_thuc_nuoi: {hinh_thuc_nuoi},
+        hs_loi_nhuan: {hs_loi_nhuan},
+        hscd_thuc_an: {hscd_thuc_an},
+        kich_co: {kich_co},
+        kich_thuoc: {kich_thuoc},
+        loai_thuc_an: {loai_thuc_an},
+        mat_do_tha: {mat_do_tha},
+
       })
     }
   }
@@ -335,75 +392,34 @@ export default function Admin() {
     }
   ]
 
-  const [slice, setSlice] = useState([])
-  const [slice2, setSlice2] = useState([])
-  const [slice3, setSlice3] = useState([])
-  const [slice4, setSlice4] = useState([])
-  const [slice5, setSlice5] = useState([])
-  const [slice6, setSlice6] = useState([])
+  const [slice, setSlice] = useState(false)
+  const [slice2, setSlice2] = useState(false)
+  const [slice3, setSlice3] = useState(false)
+  const [slice4, setSlice4] = useState(false)
+  const [slice5, setSlice5] = useState(false)
+  const [slice6, setSlice6] = useState(false)
 
-  const ListItem = () => {
-    // console.log(slice)
-    // console.log(slice2)
-    return(
-      <div style={{position:"relative", justifyContent:"center", marginLeft:"15%", marginTop:"5%"}}>
-        <h2 style={{position:"relative", color:"white", top:"-1rem"}} >{
-          Object.values(slice).length>0 ? slice[0].text :
-          Object.values(slice2).length>0 ? slice2[0].text :
-          Object.values(slice3).length>0 ? slice3[0].text :
-          Object.values(slice4).length>0 ? slice4[0].text :
-          Object.values(slice5).length>0 ? slice5[0].text :
-          Object.values(slice6).length>0 ? slice6[0].text : null}</h2>
-        <Form>
-          <FormGroup>
-          <Table className='table__css' style={{width:"80%", color:"white", position:"relative"}}>
-            <thead>
-              <tr>
-                <th>Ảnh:</th>
-                <td width="50%" style={{textAlign:'center'}}>
-                  <Input
-                    type='file'
-                    onChange={(event) => setImageUpLoad(event.target.files[0])}>
-                  </Input></td>
-              </tr>
-              <tr>   
-                <th>Tiêu Đề:</th>
-                <td>
-                  <Input
-                    type='textarea'
-                    placeholder='Nhập Tiêu Đề'
-                    onChange={(e) => setTitle(e.target.value)}>
-                  </Input></td>
-              </tr>
-              <tr>
-                <th>Nội dung:</th>
-                <td>
-                  <Input 
-                    type='textarea' 
-                    placeholder='Nhập Nội Dung'
-                    onChange={(e) => setBody(e.target.value)}>
-                  </Input>
-                </td>
-              </tr>
-            </thead>
-          </Table>
-          </FormGroup>
-          <FormGroup style={{paddingLeft:"2%", paddingTop:"3rem"}}>
-            {""}
-          </FormGroup>
-        </Form>
-        <button style={{width:"100px", height:"40px", fontSize:"18px", backgroundColor:"royalblue", borderRadius:"10px", marginBottom:"5rem"}}
-          scolor='success' onClick={
-          Object.values(slice).length>0 ? slice[0].function : 
-          Object.values(slice2).length>0 ? slice2[0].function :
-          Object.values(slice3).length>0 ? slice3[0].function :
-          Object.values(slice4).length>0 ? slice4[0].function :
-          Object.values(slice5).length>0 ? slice5[0].function :
-          Object.values(slice6).length>0 ? slice6[0].function : null} type="submit">Upload</button>
-        <hr style={{width:"80%"}} ></hr>
-      </div>
-    )
-  }
+  function handleSlice() {setSlice(true) + setSlice2(false)
+    + setSlice3(false) + setSlice4(false) + setSlice5(false) + setSlice6(false)}
+
+  function handleSlice2() {setSlice2(true) + setSlice(false)
+    + setSlice3(false) + setSlice4(false) + setSlice5(false) + setSlice6(false)}
+
+  function handleSlice3() {setSlice3(true) + setSlice(false)
+    + setSlice2(false) + setSlice4(false) + setSlice5(false) + setSlice6(false)}
+
+  function handleSlice4() {setSlice4(true) + setSlice(false)
+    + setSlice2(false) + setSlice3(false) + setSlice5(false) + setSlice6(false)}
+
+  function handleSlice5() {setSlice5(true) + setSlice(false)
+    + setSlice2(false) + setSlice3(false) + setSlice4(false) + setSlice6(false)}
+
+  function handleSlice6() {setSlice6(true) + setSlice(false)
+    + setSlice2(false) + setSlice3(false) + setSlice4(false) + setSlice5(false)}
+
+  const items = slice ? PHAN_LOAI_SX.slice(0,1) : slice2 ? PHAN_LOAI_SX.slice(1,2)
+    : slice3 ? PHAN_LOAI_SX.slice(2,3) : slice4 ? PHAN_LOAI_SX.slice(3,4)
+    : slice5 ? PHAN_LOAI_SX.slice(4,5) : slice6 ? PHAN_LOAI_SX.slice(5,6) : PHAN_LOAI_SX.slice(0,1)
   
   ///////////////////
   return (
@@ -447,29 +463,72 @@ export default function Admin() {
     </Form>
     <hr style={{position:"relative", color:"white", margin:"auto", maxWidth:"70%", marginTop:"2%"}}></hr>
 
-    <div style={{position:"relative", marginLeft:'20%'}}>
-      <button onClick={() => ListItem(setSlice(PHAN_LOAI_SX.slice(0,1)) + setSlice2([]))} style={{
-        position:"relative", color:"black", backgroundColor:"darkgrey", width:"180px", height:"50px",
-        borderTopLeftRadius:'5px', borderBottomLeftRadius:"5px"}}>CÁ BIỂN SẢN XUẤT</button>
-      <button onClick={() => ListItem(setSlice2(PHAN_LOAI_SX.slice(1,2)) + setSlice([]) + setSlice3([])
-       + setSlice4([]) + setSlice5([]) + setSlice6([]))} style={{
-        position:"relative", color:"black", backgroundColor:"darkgrey", width:"180px", height:"50px"}}>CÁ BIỂN TỰ NHIÊN</button>
-      <button onClick={() => ListItem(setSlice3(PHAN_LOAI_SX.slice(2,3)) + setSlice([]) + setSlice2([])
-       + setSlice4([]) + setSlice5([]) + setSlice6([]))} style={{
-        position:"relative", color:"black", backgroundColor:"darkgrey", width:"180px", height:"50px"}}>CÁ MÚ SẢN XUẤT</button>
-      <button onClick={() => ListItem(setSlice4(PHAN_LOAI_SX.slice(3,4)) + setSlice([]) + setSlice2([])
-       + setSlice3([]) + setSlice5([]) + setSlice6([]))} style={{
-        position:"relative", color:"black", backgroundColor:"darkgrey", width:"180px", height:"50px"}}>TÔM, CÁ GIỐNG</button>
-      <button onClick={() => ListItem(setSlice5(PHAN_LOAI_SX.slice(4,5)) + setSlice([]) + setSlice2([])
-       + setSlice3([]) + setSlice4([]) + setSlice6([]))} style={{
-        position:"relative", color:"black", backgroundColor:"darkgrey", width:"180px", height:"50px"}}>GIỐNG NHUYỄN THỂ</button>
-      <button onClick={() => ListItem(setSlice6(PHAN_LOAI_SX.slice(5,6)) + setSlice([]) + setSlice2([])
-       + setSlice3([]) + setSlice4([]) + setSlice5([]))} style={{
-        position:"relative", color:"black", backgroundColor:"darkgrey", width:"180px", height:"50px",
-        borderTopRightRadius:'5px', borderBottomRightRadius:'5px'}}>CÁ NƯỚC NGỌT</button>
+    <div style={{position:"relative", marginLeft:"20%"}}>
+      <button onClick={handleSlice} style={{position:"relative", color:'black',
+        backgroundColor:"darkgrey", width:"180px", height:"50px",
+        borderTopLeftRadius:"5px", borderBottomLeftRadius:"5px"}}>CÁ BIỂN SẢN XUẤT</button>
+      <button onClick={handleSlice2} style={{position:"relative", color:'black',
+        backgroundColor:"darkgrey", width:"180px", height:"50px"}}>CÁ BIỂN TỰ NHIÊN</button>
+      <button onClick={handleSlice3} style={{position:"relative", color:'black',
+        backgroundColor:"darkgrey", width:"180px", height:"50px"}}>CÁ MÚ SẢN XUẤT</button>
+      <button onClick={handleSlice4} style={{position:"relative", color:'black',
+        backgroundColor:"darkgrey", width:"180px", height:"50px"}}>GIỐNG TÔM, CUA</button>
+      <button onClick={handleSlice5} style={{position:"relative", color:'black',
+        backgroundColor:"darkgrey", width:"180px", height:"50px"}}>GIỐNG NHUYỄN THỂ</button>
+      <button onClick={handleSlice6} style={{position:"relative", color:'black',
+        backgroundColor:"darkgrey", width:"180px", height:"50px",
+        borderTopRightRadius:"5px", borderBottomRightRadius:"5px"}}>GIỐNG CÁ NƯỚC NGỌT</button>
     </div>
     
-    <ListItem/>
+    {items.map((menu) => {
+      return(
+        <div style={{position:"relative", justifyContent:"center", marginLeft:"15%", marginTop:"5%"}}>
+        <h2 style={{position:"relative", color:"white", top:"-1rem"}} >{menu.text}</h2>
+        <Form>
+          <FormGroup>
+          <Table className='table__css' style={{width:"80%", color:"white", position:"relative"}}>
+            <thead>
+              <tr>
+                <th>Ảnh:</th>
+                <td width="50%" style={{textAlign:'center'}}>
+                  <Input
+                    type='file'
+                    onChange={(event) => setImageUpLoad(event.target.files[0])}>
+                  </Input></td>
+              </tr>
+              <tr>
+                <th>Tiêu Đề:</th>
+                <td>
+                  <Input
+                    type='textarea'
+                    placeholder='Nhập Tiêu Đề'
+                    onChange={(e) => setTitle(e.target.value)}>
+                  </Input></td>
+              </tr>
+              <tr>
+                <th>Nội dung:</th>
+                <td>
+                  <Input 
+                    type='textarea' 
+                    placeholder='Nhập Nội Dung'
+                    onChange={(e) => setBody(e.target.value)}>
+                  </Input>
+                </td>
+              </tr>
+            </thead>
+          </Table>
+          </FormGroup>
+          <FormGroup style={{paddingLeft:"2%", paddingTop:"2rem"}}>
+            {""}
+          </FormGroup>
+        </Form>
+        <button style={{width:"100px", height:"40px", fontSize:"18px", backgroundColor:"royalblue", borderRadius:"10px", marginBottom:"5rem"}}
+             scolor='success' onClick={menu.function} type="submit">Upload</button>
+
+        <hr style={{width:"80%"}} ></hr>
+      </div>
+      )
+    })}
 
     {MENU_LIST.map((menu) => {
       return(
